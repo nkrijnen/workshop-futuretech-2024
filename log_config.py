6c45f4a099042d67f592d404f099e111ecac5e6f
Jettro Coenradie <jettro@coenradie.com>
2023-05-20 17:25:08 +0200

    Initialize empty python project

---
+++ log_config.py
@@ -0,0 +1,26 @@
+LOG_LEVEL: str = "INFO"
+FORMAT: str = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+logging_config = {
+    "version": 1, # mandatory field
+    # if you want to overwrite existing loggers' configs
+    "disable_existing_loggers": False,
+    "formatters": {
+        "basic": {
+            "format": FORMAT,
+        }
+    },
+    "handlers": {
+        "console": {
+            "formatter": "basic",
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stderr",
+        }
+    },
+    "loggers": {
+        "runner": {
+            "handlers": ["console"],
+            "level": LOG_LEVEL,
+            "propagate": False,
+        }
+    },
+}
